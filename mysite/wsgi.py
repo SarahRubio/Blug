cb14aae920e0436cd1cb145646c5dcfedbd26239
SarahRubio <sarahrubio.pro@gmail.com>
2020-09-09 17:04:30 +0200

    change config settings + wsgi

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -11,10 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from dotenv import load_dotenv
-project_folder = os.path.expanduser('~/Blug') 
-load_dotenv(os.path.join(project_folder, '.env'))
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
