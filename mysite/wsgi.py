eea68f8ec377c8b28d0e95e0088311709d262da2
SarahRubio <sarahrubio.pro@gmail.com>
2020-09-09 15:43:48 +0200

    configure wsgi add base.html

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,12 +8,10 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-from dotenv import load_dotenv
-
-project_folder = os.path.expanduser('~/blug')
-load_dotenv(os.path.join(project_folder, '.env'))
-
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
+project_folder = os.path.expanduser('~/Blug') 
+load_dotenv(os.path.join(project_folder, '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
