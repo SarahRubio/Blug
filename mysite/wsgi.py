dc3230cfca558d4cb7cf26b1814d70cfda15576e
SarahRubio <sarahrubio.pro@gmail.com>
2020-09-09 16:26:57 +0200

    fix bug

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,7 +8,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
+
 from dotenv import load_dotenv
 project_folder = os.path.expanduser('~/Blug') 
 load_dotenv(os.path.join(project_folder, '.env'))
